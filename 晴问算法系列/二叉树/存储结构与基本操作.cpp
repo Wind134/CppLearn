724a4b43270738de9c2a9ccb978d4eeae6938169
Ping <li283254184@gmail.com>
2023-03-30 22:13:53 +0800

    update defination

--- 晴问算法系列/二叉树/存储结构与基本操作.cpp
+++ 晴问算法系列/二叉树/存储结构与基本操作.cpp
@@ -9,10 +9,22 @@ struct Node
     Node* right_child;
 };
 
-Node* root = NULL;  // 根节点为空
+Node* root = nullptr;  // 根节点为空
 
 // 创建一个结点
 Node* newNode(int v)
 {
     Node* newNode = new Node;   // 申请一个node型变量的地址空间
+    newNode->data = v;
+    newNode->left_child = nullptr;
+    newNode->right_child = nullptr;
+    return newNode;
+}
+// 二叉树的查找
+void search(Node* root, int x, int newdata)
+{
+    if (!root)  return;
+    if (root->data == x)    root->data = newdata;
+    search(root->left_child, x, newdata);    
+    search(root->right_child, x, newdata);    
 }
