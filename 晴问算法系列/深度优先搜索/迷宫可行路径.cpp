dd91a4d8f71146d8132b1ec1c92421f5c23ca596
Ping <li283254184@gmail.com>
2023-04-26 15:18:03 +0800

    code review

--- 晴问算法系列/深度优先搜索/迷宫可行路径.cpp
+++ 晴问算法系列/深度优先搜索/迷宫可行路径.cpp
@@ -1,15 +1,19 @@
 /* 现有一个n * m大小的迷宫，其中1表示不可通过的墙壁，0表示平地。
 每次移动只能向上下左右移动一格（不允许移动到曾经经过的位置），且只能移动到平地上。
 求从迷宫左上角到右下角的所有可行路径的条数。 */
+/* 顺便打印出具体的路径 */
 #include <iostream>
 #include <vector>
+#include <utility>
 using namespace std;
 
 int rows, lines;
 int count = 0;
+pair<int, int> point;
 vector<vector<int>> vec(rows, vector<int>(lines));    // 定义一个迷宫数组
 vector<int> x_orient = {0, 0, -1, 1};   // 上、下、左、右移动x坐标的变化情况
 vector<int> y_orient = {1, -1, 0, 0};   // 协同上、下、左、右移动y坐标的变化情况
+vector<pair<int, int>> storage;
 
 bool isValid(const int& x, const int& y, const vector<vector<int>>& v,
     const vector<vector<int>>& visited)  // 有效的访问点
@@ -18,23 +22,43 @@ bool isValid(const int& x, const int& y, const vector<vector<int>>& v,
     y < lines && !visited[x][y] && v[x][y] == 0);
 }
 
-void DFSMaze(const int& x, const int&y, vector<vector<int>>& visited)
+void DFSMaze(const int& x, const int& y, vector<vector<int>>& visited)
 {
-    if (x == rows - 1 && y == rows - 1)
+    if (x == rows - 1 && y == rows - 1) // 所有这些都是到了最后一个点之后的操作
     {
         count ++;
+        storage.push_back({x, y});  // 添加最后一个点
+        for (const auto& data : storage)
+        {
+            if (data == pair<int, int>{rows - 1, rows - 1})
+                cout << '(' <<data.first + 1<< ", " << data.second + 1 << ')'
+                    << '\n';
+            else   cout << '(' <<data.first + 1<< ", " << data.second + 1 << ')'
+                    << "=>";         
+        }
+        storage.pop_back();
         return;
     }
+    if (isValid(x, y, vec, visited))    
+    {
+        point = {x, y};
+        storage.push_back(point);
+    }    
     visited[x][y] = 1;   // 访问了[x][y]，更改标志
+    // 我们来梳理一下这个循环的逻辑，对于接下来每个执行的DFSMaze，相应的
+    // visit[x][y]的更改都会产生作用
     for (int i = 0; i < 4; i++)
     {
         int nextX = x + x_orient[i];
         int nextY = y + y_orient[i];
-        if (isValid(nextX, nextY, vec, visited))
+        if (isValid(nextX, nextY, vec, visited))    // 有效，才进行下一个
         {
             DFSMaze(nextX, nextY, visited);
+            storage.pop_back();    
         }    
     }
+    // 这一轮走完了，为了准备下一轮继续归零
+    // 要是接下来一个DFS都没有，那说明这条路走不通，仍然要归零
     visited[x][y] = 0;  // 这一轮走完了，为了准备下一轮继续归零
 }
 
