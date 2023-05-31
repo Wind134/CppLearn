518a64a995f592d31c855ede72fdf57a7dad86c6
Ping <li283254184@gmail.com>
2023-05-31 22:22:13 +0800

    Rename

--- 刷力扣/3-寻找最长子串-数组映射.cpp
+++ 刷力扣/3-寻找最长子串-数组映射.cpp
@@ -1,8 +1,5 @@
-// 找出无重复字符的最长字串，我们先找出无重复字符的字串，再来比较长度
-// 如何找出无重复字符的字串呢？
-// 遍历string，想办法列出string字串
+/* 一个很巧妙的做法，用数组做映射 */
 
-// 上述过程是自己思考的结果，确实算不上什么比较好的思路
 #include <iostream>
 #include <vector>
 #include <math.h>
@@ -13,9 +10,7 @@ int lengthOfLongestSubstring(const string &s) {
         vector<int> m(128, 0);      // 128个ASCII码默认全部赋0，字母与数字一一对应
 
         int ans = 0;
-
         int i = 0;
-
         for (int j = 0; j < s.size(); j++) {
             i = max(i, m[s[j]]);
             m[s[j]] = j + 1;    // 当前位置加1，更新字母的位置信息
