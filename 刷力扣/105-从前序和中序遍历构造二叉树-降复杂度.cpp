c2ce03845dd60e528fff4c62938a402ef4739516
Ping <li283254184@gmail.com>
2023-07-05 14:22:25 +0800

    commit code

--- 刷力扣/105-从前序和中序遍历构造二叉树-降复杂度.cpp
+++ 刷力扣/105-从前序和中序遍历构造二叉树-降复杂度.cpp
@@ -12,31 +12,40 @@
 #include "../headfile/TreeNode.h"
 using namespace std;
 
+// 这部分用来建立映射，将中序数组中的值与下标索引相互对应
 map<int, int> elem_index;
 
+TreeNode* splitByIndex(const vector<int>& preorder, int begin_index_pre, int end_index_pre,  // pre中的起始位
+int begin_index_in, int end_index_in)    // 分别代表数组区间的起始点
+{
+    // 临界点
+    if (begin_index_pre >= preorder.size() || begin_index_pre > end_index_pre) return nullptr;
 
+    if (begin_index_pre == end_index_pre)   return (new TreeNode(preorder[begin_index_pre]));
+
+    TreeNode* root = new TreeNode(preorder[begin_index_pre]);
+
+    int index = elem_index[preorder[begin_index_pre]];   // 获取先序遍历中根节点的索引，以获取左右子树的索引
+
+    // 从这里可以直到，索引0~index - 1是左子树，index + 1~size - 1是右子树
+    root->left = splitByIndex(preorder, begin_index_pre + 1, index - begin_index_in + begin_index_pre, begin_index_in, index - 1);
+    root->right = splitByIndex(preorder, index - begin_index_in + begin_index_pre + 1, end_index_pre, index + 1, end_index_in);
+
+    return root;
+} 
 
 TreeNode* buildTree(const vector<int>& preorder, const vector<int>& inorder)
 {
     if (preorder.size() == 0)   return nullptr; // 用哪个去表示size应该都行，反正大小必定一致
 
-    if (elem_index.empty())
-    {
+    if (elem_index.empty()) {
         for (int i = 0; i < inorder.size(); i++)
-        {   
             elem_index[inorder[i]] = i;
-        }
     }
 
-    int index = elem_index[preorder[0]];    // 直接通过map寻找
-    TreeNode* root = new TreeNode(preorder[0]); // 确立根节点
+    int size = preorder.size();
 
-    vector<int> sub_pre_node_left(preorder.begin() + 1, preorder.begin() + index + 1);
-    vector<int> sub_pre_node_right(preorder.begin() + index + 1, preorder.end());
-    vector<int> sub_in_node_left(inorder.begin(), inorder.begin() + index);
-    vector<int> sub_in_node_right(inorder.begin() + index + 1, inorder.end());
-    root->left = buildTree(sub_pre_node_left, sub_in_node_left);
-    root->right = buildTree(sub_pre_node_right, sub_in_node_right);
+    TreeNode* root = splitByIndex(preorder, 0, size - 1, 0, size - 1);
 
     return root;
 }
