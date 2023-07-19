c1f5b29f270627fa3d2e73ea0cf09bc8f9348dab
Ping <li283254184@gmail.com>
2023-07-19 14:55:16 +0800

    some change

--- POSIX接口/execvp.c
+++ POSIX接口/execvp.c
@@ -8,7 +8,7 @@ int main() {
     // 调用execvp()函数执行命令，从这里可以看出来的是
     // command数组中第一个字符串一般要与执行的程序名相同，这样后面才能检测到参数
     // 因此第一个参数不用"ls"，用command[0]会更好，不需要硬编码到程序中
-    execvp("ls", command);
+    execvp(command[0], command);
 
     // 如果execvp()函数执行失败，下面的代码会被执行
     perror("execvp() failed");
