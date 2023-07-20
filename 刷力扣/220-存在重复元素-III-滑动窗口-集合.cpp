f9b7dd40fa1c98b6bfd28b2957202a24ad26c1d9
Ping <li283254184@gmail.com>
2023-07-20 18:39:18 +0800

    Leetcode Practice

--- 刷力扣/220-存在重复元素-III-滑动窗口-集合.cpp
+++ 刷力扣/220-存在重复元素-III-滑动窗口-集合.cpp
@@ -39,18 +39,32 @@ bool containsNearbyAlmostDuplicate(vector<int>& nums, int indexDiff, int valueDi
     }
 
     multiset<int> seq_set;
+
+    for_each(nums.begin(), nums.begin() + indexDiff, [&seq_set](int x) { seq_set.insert(x); });    // set应该是有序的
+    for_each(seq_set.begin(), seq_set.end(), [](int x) { cout << x << " "; });
+    for (auto it = seq_set.begin(); it != seq_set.end(); it++) {
+        if (next(it) != seq_set.end() && *next(it) - *it <= valueDiff)  return true;
+    }
+    int first_insert = nums[0];
     
-    for (int i = 0; i < size - indexDiff; i++) {
-        int left = i, right = left + indexDiff;  // 定义初始的左右指针
-        seq_set.erase(seq_set.begin(), seq_set.end());
-        for_each(nums.begin() + left, nums.begin() + right + 1, [&seq_set](int x) {seq_set.insert(x);});    // set应该是有序的
-    
-        // 那接下来就好办了，因为[left, right]区间有序，我们逐个进行判断
-        auto it_left = seq_set.begin();
-        while (next(it_left) != seq_set.end()) {
-            if (*(next(it_left)) - *it_left <= valueDiff)  return true;
-            else it_left = next(it_left);
-        }  
+    for (int i = indexDiff; i < size; i++) {
+        int right = i - 1;  // 定义初始的左右指针
+        int left = i - indexDiff; 
+
+        // 在集合中找符合要求的值
+        auto iter = seq_set.lower_bound(nums[i] - valueDiff);
+        if (iter != seq_set.end() && *iter <= nums[i] + valueDiff)  return true; 
+        // seq_set.erase(seq_set.begin(), seq_set.end());  // 时间的优化点：是不是不需要erase所有的，只需要删除最近添加的那个数即可
+        // 改进点在于缩小窗口，假设要判断的元素是x：那我们要查找的元素应该是在[x - valueDiff, x + valueDiff]之间
+        // 只需要在集合中找到大于等于(x - valueDiff)的最小值y，将y与(x + valueDiff)比较
+        
+        seq_set.erase(first_insert);
+        seq_set.insert(*(nums.begin() + i));
+        first_insert = *(nums.begin() + left + 1);
+
+        // for_each(seq_set.begin(), seq_set.end(), [](int x) { cout << x << " "; });
+
+          
     }
     return false;
 }
