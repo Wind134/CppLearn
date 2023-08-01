d39e607e7547f86768acbadb59c6ac860307bd32
Ping <li283254184@gmail.com>
2023-08-01 23:17:17 +0800

    leetcode review and classify

---
+++ 刷力扣/字符串处理/58-最后一个单词的长度-istringstream版.cpp
@@ -0,0 +1,34 @@
+/*
+题目：
+- 给你一个字符串s，由若干单词组成，单词前后用一些空格字符隔开。
+- 返回字符串中最后一个单词的长度。
+- 单词是指仅由字母组成、不包含任何空格字符的最大子字符串。
+
+思路：istringstream直接上；
+*/
+
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <string>
+using namespace std;
+
+int lengthOfLastWord(string s)
+{
+        istringstream iss(s);
+        vector<string> temp;
+        string str;
+        while(iss >> str)   temp.push_back(str);
+
+        string result = temp.back();
+        int size = result.size();
+        return size;
+}
+
+int main()
+{
+    string str;
+    getline(cin, str);
+    cout << lengthOfLastWord(str) << endl;
+    return 0;
+}
