0d9f8c8471fa05da12b4639a2ca8c6effc808c2a
Ping <li283254184@gmail.com>
2023-08-04 19:32:51 +0800

    update algorithm

--- C语言/实践/链表的处理.c
+++ C语言/实践/链表的处理.c
@@ -51,22 +51,25 @@ node* reverseList(node* head)   // 传入的头结点是要翻转的链表节点
 
 node* reverseList_new_method(node* head)   // 传入的头结点是要翻转的链表节点，该方法是直接基于链表原地修改
 {
-    node* new_head = head;
-    node* prev = head;
-    node* curr = head->next;
+    node* dummy = (node*)malloc(sizeof(node));   // 哑指针，永远指向头结点
 
-    while (curr != NULL)
+    // 假设下面这个head必不空
+    node* curr = head;    // curr的初始结点，也是永远不变的那个节点
+    dummy->next = curr;
+    while (curr->next)
     {
-        prev->next = curr->next;
-        curr->next = new_head;
-        new_head = curr;
-
-        curr = prev->next;
+        node* next_node = curr->next->next;     // ->1->2->3为例，这一步保存了3
+        curr->next->next = dummy->next;         // curr下一个结点要指向之前的头结点，也就是2指向1
+        dummy->next = curr->next;               // 设定好新的头结点
+        curr->next = next_node;                 // 接起来了
     }
-    
+
+    node* new_head = dummy->next;
+
+    free(dummy);
+
     return new_head;
 }
-
 
 
 
